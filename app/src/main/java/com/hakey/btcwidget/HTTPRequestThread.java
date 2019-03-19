c6dcd655cae98ab9ad89f7242d7cc5087e1cde8c
it-school <it@osvita.od.ua>
2019-03-19 19:35:33 +0200

    V.1

--- app/src/main/java/com/hakey/btcwidget/HTTPRequestThread.java
+++ app/src/main/java/com/hakey/btcwidget/HTTPRequestThread.java
@@ -1,8 +1,5 @@
 package com.hakey.btcwidget;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Calendar;
 
@@ -19,19 +16,20 @@ class HTTPRequestThread extends Thread{
 
         try {
             URL url = new URL(urlString);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            // HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            // con.setRequestMethod("GET");
+            //BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
-            StringBuilder response = new StringBuilder();
 
+            StringBuilder response = new StringBuilder();
+            response.append("{\"btc_usd\":{\"high\":3988.00,\"low\":3956.99,\"avg\":3980.00,\"vol\":3774491.17766,\"vol_cur\":4368.01172,\"last\":3981.60,\"buy\":3871,\"sell\":3981.701,\"updated\":1482754417}}");
+ /*
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
             in.close();
-
-            output = "Price: " + JSONParser.getPrice(response.toString())
-                    + "\n" + getTimeStamp();
+*/
+            output = "Price: " + JSONParser.getPrice(response.toString()) + "\n" + getTimeStamp();
 
         } catch (Exception e) {
             output = e.toString();
